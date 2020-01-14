c91fc33eb7eef3ba39fc68c2a223f5b34cd01742
Dmitry <dmitry.ivoninsky@gmail.com>
2020-01-14 19:28:48 +0300

    Rework fifth task

--- src/main/java/fifthTask/Main.java
+++ src/main/java/fifthTask/Main.java
@@ -1,57 +1,69 @@
 package fifthTask;
 
 import java.util.*;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static List<String> list = new ArrayList<>();
+    private static final List<Queue<String>> listOfQueues = new ArrayList<>();
+    private static Random random = new Random();
+    private static Queue<String> queue;
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        List<Queue<String>> listOfQueues = new ArrayList<>();
+
         for (int i = 0; i < 10; i++) {
             listOfQueues.add(generateQueue());
         }
 
-        long before = System.nanoTime();
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        Future<List<String>> future = executorService.submit(new Serve(listOfQueues));
-        List<String> listOfUserNames = future.get();
-        long after = System.nanoTime();
-        System.out.println("Time of executing is: " + (after - before) + " nanoseconds");
-        System.out.println("Count of usernames is: " + listOfUserNames.size());
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(new Serve());
+        }
+
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Count of usernames is: " + list.size());
 
         executorService.shutdown();
+
+//        long before = System.nanoTime();
+//        List<String> list = future.get();
+//        long after = System.nanoTime();
+//        System.out.println("Time of executing is: " + (after - before) + " nanoseconds");
+    }
+
+    private static synchronized Queue<String> getRandomQueue(List<Queue<String>> queueList) {
+        int numberOfQueue = random.nextInt(10);
+        Queue<String> queue = listOfQueues.get(numberOfQueue);
+        if (!queue.isEmpty()) {
+            return queue;
+        }
+        return getRandomQueue(listOfQueues);
     }
 
     private static Queue<String> generateQueue() {
         Queue<String> queue = new LinkedList<>();
-        Random random = new Random();
-        Integer numberOfQueue = random.nextInt(100);
-        for (int i = 0; i < 100_000; i++) {
+        int numberOfQueue = random.nextInt(100);
+        for (int i = 0; i < 10; i++) {
             queue.add("user username" + (i + 1) + "(queue" + numberOfQueue + ")");
         }
         return queue;
     }
 
-    public static class Serve implements Callable<List<String>> {
-        private List<Queue<String>> queueList;
-
-        public Serve(List<Queue<String>> queueList) {
-            this.queueList = queueList;
-        }
+    public static class Serve implements Runnable {
+        final Queue<String> randomQueue = getRandomQueue(listOfQueues);
 
         @Override
-        public List<String> call() throws Exception {
-            List<String> list = new ArrayList<>();
-            Random random = new Random();
-            Integer numberOfQueue = random.nextInt(10);
-            while (!queueList.get(numberOfQueue).isEmpty()) {
-                if (queueList.get(numberOfQueue).isEmpty()) {
-                    numberOfQueue = random.nextInt(10);
+        public void run() {
+
+            while (!randomQueue.isEmpty()) {
+                System.out.println(randomQueue.element() + " served by thread: " + Thread.currentThread().getId());
+                synchronized (randomQueue){
+                    list.add(randomQueue.poll());
                 }
-                System.out.println(queueList.get(numberOfQueue).element() + " served by thread: " + Thread.currentThread().getId());
-                list.add(queueList.get(numberOfQueue).poll());
             }
-            return list;
         }
     }
 }
